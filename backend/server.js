70825995eb9cf543be1eda06235bc7008251f6c1
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2023-04-26 03:45:14 +0530

    added features 1

--- backend/server.js
+++ backend/server.js
@@ -1,65 +1,137 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 //port
 const port = process.env.PORT || 5000;
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const cors = require('cors');
+const cors = require("cors");
 //using cors
 app.use(cors());
 
-const io = new Server(server,{
-    cors: {
-        origin: '*', // allow to connect from any origin
-        methods: ["GET", "POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*", // allow to connect from any origin
+    methods: ["GET", "POST"],
+  },
 });
 let users = [];
+let randoms = [];
+let room = null;
+let conn = [];
 
-io.on('connection', (socket) => {
-    socket.on('joinRoom', (data)=>{
-        users.push({
-            id: socket.id,
-            username :data.username,
-            room : data.room
-        });
-        let arr = (users.filter((user) => user.room == data.room))
-        console.log(arr);
-        socket.join(data.room)
-        arr ? io.in(data.room).emit('users-all',arr) : null;
-        console.log(users);
-        io.in(data.room).emit('user-joined', data.username+" has joined the chat");
-    })
+const ran = (length) => {
+  return Math.floor(Math.random() * length);
+};
 
-    socket.on('message',(data)=>{
-        let room = users.find(user => user.username === data.username);
-        if(room){
-            socket.broadcast.to(room.room).emit('new-message', {
-                username : room.username ? room.username : "user-unknown",
-                message : data.message
-            });
-        }
-        // socket.emit('users',users);
+io.on("connection", (socket) => {
+  //connect randoms
+  socket.on("connect-ran", async (data) => {
+    randoms.push(socket.id);
+    console.log(randoms);
+    io.emit("online", randoms.length);
+    if (room && room != socket.id && randoms.length>1) {
+      socket.join(room + 4);
+      console.log("user joined the room");
+      let data = {
+        msg: "you are connected with stranger",
+        room,
+      };
+      conn.push({
+        user1: room,
+        user2: socket.id,
+      });
+      io.in(room + 4).emit("status", data);
+
+      room = null;
+    } else {
+      room = socket.id;
+      socket.join(room + 4);
+      console.log("waiting for stranger");
+      let data = {
+        msg: "waiting for stranger",
+        room,
+      };
+      io.in(room + 4).emit("status", data);
+    }
+  });
+
+  socket.on("random-msg", ({ msg, room }) => {
+    console.log(msg, room);
+    socket.broadcast.to(room + 4).emit("random-receive", msg);
+  });
+
+  // join room
+  socket.on("joinRoom", (data) => {
+    users.push({
+      id: socket.id,
+      username: data.username,
+      room: data.room,
     });
+    let arr = users.filter((user) => user.room == data.room);
+    console.log(arr);
+    socket.join(data.room);
+    arr ? io.in(data.room).emit("users-all", arr) : null;
+    console.log(users);
+    io.in(data.room).emit(
+      "user-joined",
+      data.username + " has joined the chat"
+    );
+  });
 
-    socket.on("disconnect", () => {
-        let room = users.find(user => user.id == socket.id);
-        if(room){
-            socket.broadcast.to(room.room).emit('new-message', {
-                username : room.username ? room.username : "user-unknown",
-                message : "user left the chat"
-            });
-            users = users.filter(user => user.id !== socket.id);
-            let arr = (users.filter((user) => user.room == room.room))
-            io.in(room.room).emit('users-left',arr);
-            console.log(users);
+  socket.on("message", (data) => {
+    let room = users.find((user) => user.username === data.username);
+    if (room) {
+      socket.broadcast.to(room.room).emit("new-message", {
+        username: room.username ? room.username : "user-unknown",
+        message: data.message,
+      });
+    }
+    // socket.emit('users',users);
+  });
+
+  socket.on("disconnect", () => {
+    // let ran = users.find(user => user == socket.id);
+    let room = users.find((user) => user.id == socket.id);
+    if (room) {
+      socket.broadcast.to(room.room).emit("new-message", {
+        username: room.username ? room.username : "user-unknown",
+        message: "user left the chat",
+      });
+      users = users.filter((user) => user.id !== socket.id);
+      let arr = users.filter((user) => user.room == room.room);
+      io.in(room.room).emit("users-left", arr);
+      console.log(users);
+    }
+    if (randoms.includes(socket.id)) {
+      randoms = randoms.filter((user) => user !== socket.id);
+      let room = conn.filter((room) => {
+        if (room.user1 == socket.id || room.user2 == socket.id) {
+          return room;
         }
-    });
+      });
+      conn = conn.filter((room) => {
+        if (room.user1 !== socket.id && room.user2 !== socket.id) {
+          return room;
+        }
+      });
+      io.emit("online", randoms.length);
+      let data = {
+        msg: "stranger left the chat",
+        room: socket.id,
+      };
+
+      room.length > 0 && io.in(room[0].user1 + 4).emit("status", data) ;
+      console.log(conn);
+      console.log(randoms);
+    }
+  });
 });
 
-app.get('/',(req,res)=>{
-    res.send("backend !!!!");
+app.get("/", (req, res) => {
+  res.send("backend !!!!");
 });
 
-server.listen(port, ()=> console.log("Server has been started on port "+ port));
+server.listen(port, () =>
+  console.log("Server has been started on port " + port)
+);
