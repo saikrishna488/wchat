23877b56f5b4c12538fbcda2726fe4dc4a77c2f3
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2023-04-28 18:03:44 +0530

    added adsense

--- wchat/src/pages/_app.jsx
+++ wchat/src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -6,10 +7,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>wChat</title>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4956894271613471"
-     crossorigin="anonymous"></script>
+        <title>wChat</title>        
       </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4956894271613471"
+     crossorigin="anonymous"></Script>
     <Component {...pageProps} />
     </>
     
